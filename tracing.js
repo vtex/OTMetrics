fa03b06c9d2121b4e4b5c7a39d8c9ecb622fd2f1
André Martins <andre.lucas.medeir@gmail.com>
2022-10-19 11:21:49 -0300

    fix: pondo return em getFinishedSpans

--- tracing.js
+++ tracing.js
@@ -21,7 +21,5 @@ module.exports = {
       .then(() => startServer())
   },
 
-  getFinishedSpans: () => {
-    traceExporter.getFinishedSpans()
-  }
+  getFinishedSpans: () => traceExporter.getFinishedSpans()
 }
