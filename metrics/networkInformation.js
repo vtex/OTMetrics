eaec760acdf9f2ca8dfb2136f74bc558d8fc13b3
André Martins <andre.lucas.medeir@gmail.com>
2022-11-18 11:56:07 -0300

    feat: adicionando métrica sobre rede

---
+++ metrics/networkInformation.js
@@ -0,0 +1,47 @@
+import { arrow, jumpOneLine, print, tab } from '../utils.js'
+
+export function getNetworkInformation(spans) {
+
+    jumpOneLine()
+    print(arrow() + 'Informações de Rede')
+    jumpOneLine()
+
+    print(tab() + 'Para a montagem do servidor alguns processos são executados para garantir o funcionamento.')
+
+    let networkSpans = [
+        { name: 'DNS',  spanName: 'dns.lookup'},
+        { name: 'TCP',  spanName: 'tcp.connect'},
+        { name: 'TLS', spanName: 'tls.connect'}
+    ]
+
+    networkSpans.forEach(({ name, spanName }) => {
+        
+        jumpOneLine()
+
+        let span = getSpanByName(spans, spanName)
+        
+        if(span) {    
+            print(tab() + dot() + name)
+            printNetworkAttibures(span)
+        } else {
+            print(tab() + dot() + 'Não foi encontrado nenhum trace sobre', name)
+        }
+        
+    })
+
+}
+
+function printNetworkAttibures(span) {
+    Object.entries(span.attributes).forEach(([ key, value ]) => {
+        print(tab(3) + key, value)
+    })
+}
+
+function getSpanByName(spans, name) {
+    for( let span of spans ) {
+        if(span.name === name) {
+            return span
+        }
+    }
+}
+
